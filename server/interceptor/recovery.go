899755883cee79153baf19c18fd3aa5f7e6b83d0
izumin5210 <m@izum.in>
2017-11-03 20:30:42 +0900

    Report panics

--- server/interceptor/recovery.go
+++ server/interceptor/recovery.go
@@ -6,13 +6,15 @@ import (
 	"github.com/creasty/apperrors"
 	"github.com/grpc-ecosystem/go-grpc-middleware/recovery"
 	"google.golang.org/grpc"
+
+	"github.com/izumin5210-sandbox/grpc-and-gateway-sample-app-go/type/system"
 )
 
 var recoveryHandler = grpc_recovery.WithRecoveryHandler(func(p interface{}) error {
 	if err, ok := p.(error); ok {
-		return apperrors.Wrap(err)
+		return system.ErrorUnknown.WithReport(err)
 	}
-	return apperrors.New(fmt.Sprint(p))
+	return system.ErrorUnknown.WithReport(apperrors.New(fmt.Sprint(p)))
 })
 
 func recoveryStreamServerInterceptor() grpc.StreamServerInterceptor {
