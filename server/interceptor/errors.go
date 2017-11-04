72e54b92e9c920546cf66512330232c02ad44c0c
izumin5210 <m@izum.in>
2017-11-05 02:37:26 +0900

    Send contextual error reports

--- server/interceptor/errors.go
+++ server/interceptor/errors.go
@@ -1,12 +1,19 @@
 package interceptor
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 
 	"github.com/creasty/apperrors"
+	"github.com/getsentry/raven-go"
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/golang/protobuf/proto"
+	"github.com/grpc-ecosystem/go-grpc-middleware/tags"
 	"github.com/izumin5210/grpc-errors"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
 
 	"github.com/izumin5210-sandbox/grpc-and-gateway-sample-app-go/type/system"
 )
@@ -21,27 +28,70 @@ var grpcCodeBySystemCode = map[system.ErrorCode]codes.Code{
 	system.ErrorFailedToWriteDB: codes.Internal,
 }
 
-var errorHandleFuncs = []grpcerrors.ErrorHandlerFunc{
-	grpcerrors.WithNotWrappedErrorHandler(func(_ context.Context, err error) error {
-		return system.ErrorUnknown.WithReport(err)
-	}),
-	grpcerrors.WithReportableErrorHandler(func(_ context.Context, err *apperrors.Error) error {
-		// TODO: Should send error reports
+var jsonPbMarshaller = &jsonpb.Marshaler{}
+
+var notWrappedErrorHandler = grpcerrors.WithNotWrappedErrorHandler(func(_ context.Context, err error) error {
+	return system.ErrorUnknown.WithReport(err)
+})
+
+var statusCodeMapper = grpcerrors.WithStatusCodeMapper(func(code int) codes.Code {
+	grpcCode, ok := grpcCodeBySystemCode[system.ErrorCode(code)]
+	if !ok {
+		return codes.Unknown
+	}
+	return grpcCode
+})
+
+var unaryServerErrorHandlers = []grpcerrors.UnaryServerErrorHandler{
+	notWrappedErrorHandler,
+	grpcerrors.WithUnaryServerReportableErrorHandler(func(c context.Context, req interface{}, info *grpc.UnaryServerInfo, err *apperrors.Error) error {
+		st := &raven.Stacktrace{}
+		for _, t := range err.StackTrace {
+			f := &raven.StacktraceFrame{
+				Filename: t.File,
+				Function: t.Func,
+				Lineno:   int(t.Line),
+			}
+			// Sentry wants the frames with the oldest first, so reverse them
+			st.Frames = append([]*raven.StacktraceFrame{f}, st.Frames...)
+		}
+		pckt := raven.NewPacket(err.Error(), st)
+		if p, ok := req.(proto.Message); ok {
+			jbuf := &bytes.Buffer{}
+			if err := jsonPbMarshaller.Marshal(jbuf, p); err == nil {
+				var data map[string]interface{}
+				if err := json.Unmarshal(jbuf.Bytes(), &data); err == nil {
+					pckt.Extra["body"] = data
+				}
+			}
+		}
+		if md, ok := metadata.FromIncomingContext(c); ok {
+			pckt.Extra["metadata"] = md
+		}
+		pckt.Extra["context_tags"] = grpc_ctxtags.Extract(c)
+
+		raven.Capture(pckt, map[string]string{
+			"method": info.FullMethod,
+		})
 		return err
 	}),
-	grpcerrors.WithStatusCodeMapper(func(code int) codes.Code {
-		grpcCode, ok := grpcCodeBySystemCode[system.ErrorCode(code)]
-		if !ok {
-			return codes.Unknown
-		}
-		return grpcCode
+	statusCodeMapper,
+}
+
+var streamServerErrorHandlers = []grpcerrors.StreamServerErrorHandler{
+	notWrappedErrorHandler,
+	grpcerrors.WithReportableErrorHandler(func(_ context.Context, err *apperrors.Error) error {
+		// TODO: wanna report errors with request contexts...
+		raven.CaptureError(err, map[string]string{})
+		return err
 	}),
+	statusCodeMapper,
 }
 
 func errorsStreamServerInterceptor() grpc.StreamServerInterceptor {
-	return grpcerrors.StreamServerInterceptor(errorHandleFuncs...)
+	return grpcerrors.StreamServerInterceptor(streamServerErrorHandlers...)
 }
 
 func errorsUnaryServerInterceptor() grpc.UnaryServerInterceptor {
-	return grpcerrors.UnaryServerInterceptor(errorHandleFuncs...)
+	return grpcerrors.UnaryServerInterceptor(unaryServerErrorHandlers...)
 }
